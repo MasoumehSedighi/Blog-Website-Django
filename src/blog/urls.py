512a56fea21fc60bfdf97001d222ea3eb89d7c9c
MasoumehSedighi <mas_cutt4@yahoo.com>
2023-11-13 13:04:13 +0330

    urls header

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'blog'
 
 urlpatterns = [
-    path('home', views.blog_home_view),
-    path('single', views.blog_single_view)
+    path('home', views.blog_home_view, name='blog-home'),
+    path('single', views.blog_single_view, name='blog-single')
     
 ]
